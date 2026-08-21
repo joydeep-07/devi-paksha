22e311e03149a63002542bd3848b8e89b118bcbd
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-08-22 00:02:22 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-screen '>
+        <img className='w-screen h-screen object-cover' src="./images/night.png" alt="" />
+    </div>
+  )
+}
+
+export default Home
