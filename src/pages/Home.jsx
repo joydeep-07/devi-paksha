3c2d83ec2ce4fd9bd8890a67bd099483563140aa
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-08-22 00:46:05 +0530

    Font Applied

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,36 @@ import MusicPlayer from "../components/MusicPlayer";
 
 const Home = () => {
   return (
-    <div className="h-screen w-full overflow-hidden">
-        <Navbar/>
+    <div className="relative h-screen w-full overflow-hidden">
+      <Navbar />
+
+      <h1
+        className="
+          absolute
+          top-[100px]
+          left-1/2
+          z-10
+          -translate-x-1/2
+          whitespace-nowrap
+          text-6xl
+          font-light
+          text-amber-300
+          md:text-7xl
+          lg:text-[130px]
+          font-bengali
+        "
+      >
+        দুগ্গা এলো
+      </h1>
+
       <img
         src="./images/night.png"
         loading="lazy"
         alt="dugga dugga"
         className="h-full w-full object-cover object-left md:object-left lg:object-center"
       />
-      <MusicPlayer/>
+
+      <MusicPlayer />
     </div>
   );
 };
