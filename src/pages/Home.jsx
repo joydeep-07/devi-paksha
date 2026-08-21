fa63f0aa9a77bd2bae54f1a2239ab30be1a25f10
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-08-22 00:05:19 +0530

    Home page created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='h-screen '>
-        <img className='w-screen h-screen object-cover' src="./images/night.png" alt="" />
+    <div className="h-screen w-full overflow-hidden">
+      <img
+        src="./images/night.png"
+        loading="lazy"
+        alt="dugga dugga"
+        className="h-full w-full object-cover object-left md:object-left lg:object-center"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
